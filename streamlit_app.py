2f7e639004445c42bea56b6d6a018ce175c6bb0b
xpara <heechang@xparadigms.com>
2023-08-18 16:18:53 +0900

    add deepl translation code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,37 +1,71 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import requests
+import json
+import toml
 
-"""
-# Welcome to Four Pillars DeepL Translator!
+# Load configuration from TOML file
+config = toml.load("config.toml")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Extract the API key from the configuration
+deepL_api_key = config["deepL"]["deepl_api_key"]
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-"""
+# Define the translation function
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+def translate_with_deepL(text: str):
+    url = "https://api-free.deepl.com/v2/translate"
+    payload = {"text": text, "target_lang": "KO"}
+    headers = {
+        "Authorization": f"DeepL-Auth-Key {deepL_api_key}",
+        "User-Agent": "YourApp/1.2.3",
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    response = requests.post(url, data=payload, headers=headers)
 
-    points_per_turn = total_points / num_turns
+    if response.status_code == 200:
+        return response.text
+    else:
+        return None
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-                    .mark_circle(color='#0068c9', opacity=0.5)
-                    .encode(x='x:Q', y='y:Q'))
+# Streamlit app title
+st.title("Markdown Translation with DeepL")
+
+# Upload a Markdown file
+markdown_file = st.file_uploader("Upload a Markdown file", type=["md"])
+
+if markdown_file is not None:
+    markdown_text = markdown_file.read().decode("utf-8")
+
+    # Display uploaded Markdown content
+    st.markdown('<div style="background-color: orange; padding: 10px;"><h3  style="color: black;">1. Uploaded Markdown Content</h3></div>',
+                unsafe_allow_html=True)
+    st.markdown(markdown_text, unsafe_allow_html=True)
+
+    # Edit the uploaded Markdown content
+    # st.subheader("Edit Markdown Content")
+    # Adjust the height as needed
+    st.markdown('<div style="background-color: orange; padding: 10px;"><h3  style="color: black;">2. Edit Markdown Content</h3></div>',
+                unsafe_allow_html=True)
+
+    edited_markdown = st.text_area(
+        "", markdown_text, height=800)
+    st.markdown('<div style="background-color: orange; padding: 10px;"><h3  style="color: black;">3. Translate with DeepL</h3></div>',
+                unsafe_allow_html=True)
+    st.markdown("")
+    # Button to trigger translation
+    # HTML button with inline styling for desired appearance
+
+    # Button to trigger translation
+    if st.button("Translate with DeepL"):
+        # Translate edited Markdown content
+        translated_text = translate_with_deepL(edited_markdown)
+
+        # Extract translation result from JSON
+        data = json.loads(translated_text)
+        translated_result = data['translations'][0]['text']
+
+        # Display translated content
+        st.subheader("Translated Content")
+        st.markdown(translated_result, unsafe_allow_html=True)
